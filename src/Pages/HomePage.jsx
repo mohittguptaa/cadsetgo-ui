2036d7741ffa59b43e1b590404c2e03da9d995ed
mohittguptaa <mohitgupta8218@gmail.com>
2025-09-18 16:01:44 +0530

    Add loadign

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSection from "../component/HeroSection";
 import WhyChoose from "../component/WhyChoose";
 import ServicesSection from "../component/ServicesSection";
 import InfoSection from "../component/InfoSection";
 import CTASection from "../component/CTASection";
+import LoadingScreen from "../component/LoadingScreen";
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 1.2 sec ke baad loading hat jaye
+    const timer = setTimeout(() => setLoading(false), 1200);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) return <LoadingScreen />;
+
   return (
     <div className="bg-black">
       <HeroSection />
       <ServicesSection />
       <WhyChoose />
       <InfoSection />
-      {/* <InfoSection
-        title="CadSetGo makes designs for the world"
-        text="Delivering high-precision CAD solutions for industries shaping
-        the future, bringing efficiency and technical excellence."
-        img="/hero-banner.jpg"
-        reverse
-      /> */}
-      <CTASection/>
+      <CTASection />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "spin-reverse-slow": "spin-reverse 3s linear infinite",
+      },
+      keyframes: {
+        "spin-reverse": {
+          from: { transform: "rotate(360deg)" },
+          to: { transform: "rotate(0deg)" },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
-
+};
